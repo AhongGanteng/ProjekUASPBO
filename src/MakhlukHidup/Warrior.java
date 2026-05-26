37f7a6b1e50ca88a2506198cf918c3029b6396b6
AhongGanteng <ahong28gg@gmail.com>
2026-05-26 09:03:20 +0700

    Update Warrior.java
    Update Warrior -Farrel

--- src/MakhlukHidup/Warrior.java
+++ src/MakhlukHidup/Warrior.java
@@ -3,71 +3,88 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package MakhlukHidup;
+
 import Interface.*;
 import System.*;
+
 /**
- * salah satu anak dari class hero, 
- * senjatanya sword, 
- * punya pasif jika darah dibawah 50% maka ketahanan damagenya meningkat +5, 
- * dan punya skill yaitu damage +50% + heal 10 darah dengan cooldown 3 turn
+ * salah satu anak dari class hero, senjatanya sword, punya pasif jika darah
+ * dibawah 50% maka ketahanan damagenya meningkat +5, dan punya skill yaitu
+ * damage +50% + heal 10 darah dengan cooldown 3 turn
  */
-public class Warrior extends Hero{
+public class Warrior extends Hero {
+
     private boolean darahTersisaSetengah; //cek darahnya
+
     /**
-     * nama player
-     * darah warrior = 150
-     * damage serangan = 18
-     * ketahanan = 12
-     * jeda penggunaan skill = 3
-     * @param nama 
+     * nama player darah warrior = 150 damage serangan = 18 ketahanan = 12 jeda
+     * penggunaan skill = 3
+     *
+     * @param nama
      */
     public Warrior(String nama) {
         super(nama, 150, 18, 12, "Warrior", 3);
         this.darahTersisaSetengah = false;
     }
+
     /**
      * damage serangan yang diberikan oleh player
+     *
      * @param target, targetnya adalah obyek Enemy
      */
     @Override
-    public void serang (Attackable target) {
-        
+    public void serang(Attackable target) {
+
     }
+
     /**
-     * Skill yang dimiliki oleh class Warrior
-     * peningkatan damage +50% dan heal 10% darah player
+     * Skill yang dimiliki oleh class Warrior peningkatan damage +50% dan heal
+     * 10% darah player
+     *
      * @param target, target serangan
      */
     @Override
     public void useSkill(Attackable target) {
-        
+
     }
+
     /**
      * Nama skill untuk class Warrior
+     *
      * @return nama skill nya
      */
-    @Override 
+    @Override
     public String getNamaSkill() {
         return null;
     }
+
     /**
      * hal yang bisa mengaktifkan skill pasif class Warrior
      */
     @Override
-    public void triggerPasif(){
-        
+    public void triggerPasif() {
+
     }
+
     /**
      * nama pasifnya
      */
-    @Override 
+    @Override
     public void getNamaPasif() {
-        
+
     }
+
     //tampilkan deskripsi player
     @Override
     public String getDeskripsi() {
-        return null;
+        return String.format("[Warrior] %s | LV %d | HP: %d/%d | ATK: %d | DEF: %d | %s",
+                getNama(),
+                getLevel().getLevel(),
+                getHp(),
+                getMaxHp(),
+                getSerangan() + getPenyimpanan().getWeaponAtkBonus(),
+                getKetahanan() + getPenyimpanan().getArmorDefBonus(),
+                (getHp() < getMaxHp() * 0.5) ? "PASSIVE ACTIVE" : "");
     }
 
     @Override
