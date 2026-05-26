8e6879d0eea254b74857221bd4a0837c7a5b3db4
BenedictusFarrel117 <farreluhud0204@gmail.com>
2026-05-26 08:57:50 +0700

    menambah getDeskripsi di archer dan enemy(abstrak), melengkapi getDeskripsi di semua folder mahkluk hidup

--- src/MakhlukHidup/Monster.java
+++ src/MakhlukHidup/Monster.java
@@ -1,39 +1,51 @@
 package MakhlukHidup;
+
 import Interface.*;
 import Items.Item;
+
 /**
  *
  * @author LENOVO
  */
-public class Monster extends Enemy{
+public class Monster extends Enemy {
+
     public Monster(int hadiahExp, Item[] dropItem, double dropChange, int lantai, String nama, int maxHp, int serangan, int ketahanan) {
         super(hadiahExp, dropItem, dropChange, lantai, nama, maxHp, serangan, ketahanan);
-   
-    
-}
+
+    }
+
     @Override
     public void serang(Attackable target) {
         int damage = Math.max(1, getSerangan());
-        
+
         System.out.println(getNama() + "menyerang");
         target.kenaDamage(damage);
-        
+
     }
-    
+
     @Override
     public String tipeMusuh() {
         return "Moster";
     }
-    
+
     @Override
     public String getDeskripsi() {
-        return getNama() + "adalah monster dari lantai"+ getLantai();
+        return String.format("[Monster] %s (Floor %d) | HP: %d/%d | ATK: %d | DEF: %d | EXP: %d | Drop Rate: %.0f%%",
+                getNama(),
+                getLantai(),
+                getHp(),
+                getMaxHp(),
+                getSerangan(),
+                getKetahanan(),
+                getHadiahExp(),
+                getDropChange() * 100);
     }
+
     @Override
     public boolean isAlive() {
         return getHp() > 0;
     }
-    
+
     @Override
     public void showStatus() {
         System.out.println("\nStatus Monster");
